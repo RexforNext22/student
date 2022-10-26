5f0bf1914a2ccf0ce0e6bdaf76931f59f408b97e
mr_ryan <61605919+RexforNext22@users.noreply.github.com>
2022-10-26 17:55:50 -0600

    Added by Ryan

--- studentpages/urls.py
+++ studentpages/urls.py
@@ -1,11 +1,15 @@
 # Import the django.url and views function
 from django.urls import path
-from .views import indexPageView
+from .views import UpdatePageView, indexPageView
 from .views import libraryPageView
 from .views import newPageView
 from .views import aboutPageView
 from .views import savePageView
 from .views import showSingleStudent
+from .views import EditPageView
+from .views import UpdatePageView
+from .views import DeletePageView
+
 
 
 
@@ -17,5 +21,8 @@ urlpatterns = [
     path("new/", newPageView, name="new"),
     path("about/", aboutPageView, name="about"),
     path("save/", savePageView, name="save"),
-    path("showSingleStudent/<int:id>/", showSingleStudent, name="showSingleStudent")                 
+    path("showSingleStudent/<int:id>/", showSingleStudent, name="showSingleStudent"),
+    path("edit/<int:id>", EditPageView, name="edit"),
+    path("update/<int:id>", UpdatePageView, name="update"),     
+    path("delete/<int:id>", DeletePageView, name="delete"),            
         ]        
--- studentpages/views.py
+++ studentpages/views.py
@@ -70,3 +70,58 @@ def showSingleStudent(request, id):
     }
 
     return render(request, 'studentpages/showSingleStudent.html', context)
+
+def EditPageView(request, id):
+    data = Student.objects.get(id=id)
+
+    context = {
+        "Student": data,
+    }
+
+    return render(request, 'studentpages/edit.html', context)
+
+def UpdatePageView(request, id):
+    if request.method == 'POST':
+        oStudent = Student.objects.get(id=id)
+
+        oStudent.first_name = request.POST.get('first_name')
+        oStudent.last_name = request.POST.get('last_name')
+        oStudent.bInternational = request.POST.get('bInternational')
+        oStudent.gender = request.POST.get('gender')
+        oStudent.email = request.POST.get('email')
+        oStudent.fExpectedHours = request.POST.get('fExpectedHours')
+        oStudent.sSemester = request.POST.get('sSemester')
+        oStudent.iYear = request.POST.get('iYear')
+        oStudent.sPhone = request.POST.get('sPhone')
+        oStudent.sBYUID = request.POST.get('sBYUID')
+        oStudent.sPositionType = request.POST.get('sPositionType')
+        oStudent.sClassCode = request.POST.get('sClassCode')
+        oStudent.iEmployeeRecord = request.POST.get('iEmployeeRecord')
+        oStudent.sSupervisior = request.POST.get('sSupervisior')
+        oStudent.dDate = request.POST.get('dDate')
+        oStudent.fPayRate = request.POST.get('fPayRate')
+        oStudent.dLastPayIncrease = request.POST.get('dLastPayIncrease')
+        oStudent.fIncreaseAmount = request.POST.get('fIncreaseAmount')
+        oStudent.dIncreaseInput = request.POST.get('dIncreaseInput')
+        oStudent.sYearInProgram = request.POST.get('sYearInProgram')
+        oStudent.bPayGradTuition = request.POST.get('bPayGradTuition')
+        oStudent.bNameChangeCompleted = request.POST.get('bNameChangeCompleted')
+        oStudent.sNotes = request.POST.get('sNotes')
+        oStudent.bTerminated = request.POST.get('bTerminated')
+        oStudent.dTerminationDate = request.POST.get('dTerminationDate')
+        oStudent.bQualtricsSurveySent = request.POST.get('bQualtricsSurveySent')
+        oStudent.bSubmittedEForm = request.POST.get('bSubmittedEForm')
+        oStudent.dEFormSubmitted = request.POST.get('dEFormSubmitted')
+        oStudent.bAuthroizedToWork = request.POST.get('bAuthroizedToWork')
+        oStudent.dAuthorizationWorkEmailSent = request.POST.get('dAuthorizationWorkEmailSent')
+        oStudent.sBYUName = request.POST.get('sBYUName')
+
+
+        oStudent.save()
+
+    return indexPageView(request)
+
+def DeletePageView(request, id):
+    data = Student.objects.get(id=id)
+    data.delete()
+    return indexPageView(request)
